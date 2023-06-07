23c485865e58b66596807fba94c726b22de7e4dd
Errol Gamsu <errolgamsu@Errols-MacBook-Pro.local>
2023-06-07 15:21:37 +0300

    6

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
+import useWordGame from "./hooks/useWordGame";
 
-function App() {
-  const initialTimeRemaining = 5;
-  const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(1);
-  const [gameRunning, setGameRunning] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-  const textBoxRef = useRef(null);
-
-  useEffect(() => {
-    if (gameRunning && timeRemaining > 0) {
-      setTimeout(() => {
-        setTimeRemaining((time) => time - 1);
-      }, 1000);
-    } else if (timeRemaining === 0) {
-      setGameRunning(false);
-      setWordCount(calculateWordCount(text));
-    }
-  }, [timeRemaining, gameRunning]);
-
-  const resetCount = () => {
-    setTimeRemaining(initialTimeRemaining);
-    setGameRunning((prev) => !prev);
-    setText("");
-    textBoxRef.current.disabled = false;
-    textBoxRef.current.focus();
-  };
-
-  function handleOnClick() {
-    setGameRunning((prev) => !prev);
-  }
-
-  function handleChange(e) {
-    const { value } = e.target;
-    setText(value);
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ");
-    return wordsArr.filter((word) => word !== "").length;
-  }
+function App(props) {
+  const {
+    handleChange,
+    text,
+    gameRunning,
+    textBoxRef,
+    timeRemaining,
+    resetCount,
+    wordCount,
+  } = useWordGame();
 
   return (
     <div>
